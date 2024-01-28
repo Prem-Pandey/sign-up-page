739b9128c6aded0d9a8e4f9c462c7622d626c91e
Prem-Pandey <dev.premrpandey@gmail.com>
2024-01-28 17:43:56 +0530

    adding expense features

--- app.js
+++ app.js
@@ -36,8 +36,29 @@ const User = sequelize.define('User', {
   },
 });
 
-// Sync the model with the database
-User.sync();
+const Expense = sequelize.define('Expense', {
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database synced successfully');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
+// User.sync();
 
 app.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, '/signUp.html'));
@@ -106,6 +127,58 @@ app.post('/login', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+app.post('/addExpense', async (req, res) => {
+  const { amount, description, category } = req.body;
+
+  try {
+    // Create a new expense
+    const newExpense = await Expense.create({
+      amount,
+      description,
+      category,
+    });
+
+    res.status(201).send('Expense added successfully');
+  } catch (error) {
+    console.error('Error adding expense:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/getAllExpenses', async (req, res) => {
+  try {
+    // Fetch all expenses from the database
+    const allExpenses = await Expense.findAll();
+
+    res.status(200).json(allExpenses);
+  } catch (error) {
+    console.error('Error fetching expenses:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.delete('/deleteExpense/:expenseId', async (req, res) => {
+  const expenseId = req.params.expenseId;
+
+  try {
+    // Find the expense by ID and delete it
+    const deletedExpense = await Expense.destroy({
+      where: { id: expenseId },
+    });
+
+    if (deletedExpense) {
+      res.status(200).send('Expense deleted successfully');
+    } else {
+      res.status(404).send('Expense not found');
+    }
+  } catch (error) {
+    console.error('Error deleting expense:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+   
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
